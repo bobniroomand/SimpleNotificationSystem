459410b0e21c551a23c4478bec24bef91774376d
Babak Niroomand <babakniroomand@hotmail.com>
2018-09-17 02:21:17 +0430

    added condition for null notofications

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -15,19 +15,24 @@ namespace ClientApp
         static void Main(string[] args)
         {
             int numberOfExecutedTasks = 0;
-            for (int i = 0; i < 200; i++)
+            int numberOfRequests = 50;
+            for (int i = 0; i < numberOfRequests; i++)
             {
                 int c = i;
                 ThreadPool.QueueUserWorkItem((obj) =>
                             {
                                 Thread.CurrentThread.IsBackground = false;
                                 var client = OpenConnectionToServer();
-                                var notifs = client.GetNotifications();
-                                Console.WriteLine("notifs of client #{0}: {1}",c ,notifs);
+                                var notifications = client.GetNotifications();
+                                if (notifications == null)
+                                {
+                                    Console.WriteLine("couldn't get notification for client #{0}", c);
+                                }
+                                Console.WriteLine("notifs of client #{0}: {1}",c ,notifications);
                                 numberOfExecutedTasks++;
                             });
             }
-            while (numberOfExecutedTasks < 200) ;
+            while (numberOfExecutedTasks < numberOfRequests) ;
         }
         static NotificationService.Client OpenConnectionToServer()
         {
